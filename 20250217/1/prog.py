dbf7f24a7a3a625e13cc96b0b567647a30cb5bdb
LIO <ofleuxin@gmail.com>
2025-02-21 12:30:00 +0300

    HW for 17.02 2

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -3,11 +3,18 @@ import os
 import zlib
 import glob
 
-
 if len(sys.argv) == 1:
-	print("There are too few arguments.")
+    print("There are too few arguments.")
+    exit(0)
 
-elif len(sys.argv) == 2:
+path = sys.argv[1] + '/.git/refs/heads'
+
+
+if len(sys.argv) == 2:
+    for item in glob.iglob(path):
+        direc = os.scandir(item)
+        for branch in direc:
+            print(branch.name)
 	pass
 
 else:
