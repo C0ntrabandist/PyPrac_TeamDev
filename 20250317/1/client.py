05993e32dd06d4339036d452edb05cd628afdeb4
LIO <ofleuxin@gmail.com>
2025-03-19 10:12:40 +0300

    Add client

--- 20250317/1/client.py
+++ 20250317/1/client.py
@@ -1,8 +1,13 @@
 import cowsay
 import cmd
 from io import StringIO
+import sys
+import socket
 import shlex
 
+
+host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
 
 class Mud(cmd.Cmd):
     jgsbat = cowsay.read_dot_cow(StringIO("""
@@ -19,38 +24,45 @@ class Mud(cmd.Cmd):
     EOC
     """))
 
-    weapons = ['sword', 'spear', 'axe']
+    weapons = {'sword': 10, 'spear': 15, 'axe': 20}
 
     prompt = ':->'
 
-    def __init__(self):
+    def __init__(self, conn):
         super().__init__()
+
+        self.conn = conn
 
         self.x = 0
         self.y = 0
 
         self.field = [[0 for j in range(10)] for i in range(10)]
 
-        self.default = ('', '', 0, -1, -1)
-
+        self.invalid_mon = ('', '', 0, -1, -1)
+        # self.default_mon = ('default', 'Hello', 100, 0, 0)
         self.allowed_list = cowsay.list_cows()
         self.user_list = {'jgsbat': self.jgsbat}
-
 
     def get_mon_args(self, args):
         args = shlex.split(args)
 
-        name, hello, hp, m_x, m_y  = self.default
+        name, hello, hp, m_x, m_y  = self.invalid_mon
+        if len(args) == 0:
+            args += ['default']
 
         if len(args) != 8:
-            print("Invalid arguments")
-            return self.default
+            if "coords" not in args:
+                args += ['coords', 0, 0]
+            if "hp" not in args:
+                args += ["hp", 100]
+            if "hello" not in args:
+                args += ["hello", "Hello"]
 
         name = args[0]
 
         if name not in self.allowed_list and name not in self.user_list:
             print("Invalid arguments")
-            return self.default
+            return self.invalid_mon
 
         i = 1
         while i < 8:
@@ -60,36 +72,39 @@ class Mud(cmd.Cmd):
                 try:
                     hp = int(args[i+1])
                 except Exception:
-                    return self.default
+                    return self.invalid_mon
 
                 if hp <= 0:
-                    return self.default
+                    return self.invalid_mon
             elif args[i] == 'coords':
                 try:
                     m_x = int(args[i+1])
                     m_y = int(args[i+2])
                 except Exception:
-                    return self.default
+                    return self.invalid_mon
 
                 if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
                     print("Invalid arguments")
-                    return self.default
+                    return self.invalid_mon
 
                 i += 1
             else:
                 print("Invalid arguments")
-                return self.default
+                return self.invalid_mon
             i += 2
 
         if i < 8:
-            return self.default
+            return self.invalid_mon
 
         return (name, hello, hp, m_x, m_y)
 
-
+    '''
     def move_mon(self, name, hello, hp, m_x, m_y):
-        if (name, hello, hp, m_x, m_y) == self.default:
+        if (name, hello, hp, m_x, m_y) == self.invalid_mon:
             return
+
+        self.conn.sendall(f"addmon {name} {m_x} {m_y} {hp} {hello}\n".encode())
+        data = self.conn.recv(1024).decode().split()
 
         if self.field[m_y][m_x] == 0:
             print(f'Added monster to ({m_x}, {m_y}) saying {hello}')
@@ -97,63 +112,45 @@ class Mud(cmd.Cmd):
             print(f'Replaced the old monster')
 
         self.field[m_y][m_x] = {'hello':hello, 'hp': hp, 'name': name}
+    '''
 
+    def encounter(self, dx, dy):
+        self.conn.sendall(f"move {dx} {dy}\n".encode())
+        data = self.conn.recv(1024).decode().split()
 
-    def encounter(self, x, y):
+        x, y = data[1], data[2]
         print(f'Moved to ({x}, {y})')
 
-        if self.field[y][x] == 0:
-            return
-
-        hello = self.field[y][x]['hello']
-        hp = self.field[y][x]['hp']
-        name = self.field[y][x]['name']
-
-        if name in self.allowed_list:
-            print(cowsay.cowsay(hello, cow=name))
-        else:
-            print(cowsay.cowsay(hello, cowfile=self.user_list[name]))
-
-
-    def move_up(self):
-        self.y = (self.y - 1) % 10
-        self.encounter(self.x, self.y)
-
-
-    def move_down(self):
-        self.y = (self.y + 1) % 10
-        self.encounter(self.x, self.y)
-
-
-    def move_right(self):
-        self.x = (self.x + 1) % 10
-        self.encounter(self.x, self.y)
-
-
-    def move_left(self):
-        self.x = (self.x - 1) % 10
-        self.encounter(self.x, self.y)
-
+        if data[0] == 'monster' and data[3] in self.allowed_list:
+            print(cowsay.cowsay(" ".join(data[4:]), cow=data[3]))
+        elif data[0] == 'monster':
+            print(cowsay.cowsay(" ".join(data[4:]), cowfile=self.user_list[data[3]]))
 
     def do_up(self, args):
-        self.move_up()
-
+        self.encounter(0, -1)
 
     def do_down(self, args):
-        self.move_down()
-
+        self.encounter(0, 1)
 
     def do_right(self, args):
-        self.move_right()
-
+        self.encounter(1, 0)
 
     def do_left(self, args):
-        self.move_left()
-
+        self.encounter(-1, 0)
 
     def do_addmon(self, args):
-        self.move_mon(*self.get_mon_args(args))
+        (name, hello, hp, m_x, m_y) = self.get_mon_args(args)
 
+        if (name, hello, hp, m_x, m_y) == self.invalid_mon:
+            return
+
+        self.conn.sendall(f"addmon {name} {m_x} {m_y} {hp} {hello}\n".encode())
+        data = self.conn.recv(1024).decode().split()
+
+        if data[0] == "add":
+            print(f'Added monster to ({m_x}, {m_y}) saying {hello}')
+        else:
+            print("Replaced the old monster")
 
     def do_attack(self, args):
         args = shlex.split(args)
@@ -162,50 +159,40 @@ class Mud(cmd.Cmd):
             print("Type args")
             return
 
-        if self.field[self.y][self.x] == 0:
-            print("No monster here")
+        name = args[0]
+        if name not in self.allowed_list and name not in self.user_list:
+            print("No such monster")
             return
-
-        if args[0] != self.field[self.y][self.x]['name']:
-            print(f"No {args[0]} here")
-
-        weapon = 'sword'
 
         if len(args) >= 2 and args[1] != 'with':
             print("Invalid arguments")
             return
 
+        weapon = 'sword'
         if len(args) >= 3:
             weapon = args[2]
 
-        if weapon != 'sword' and weapon != 'spear' and weapon != 'axe':
+        if weapon not in self.weapons:
             print("Unknown weapon")
             return
 
-        hp = int(self.field[self.y][self.x]['hp'])
-        name = self.field[self.y][self.x]['name']
+        damage = self.weapons[weapon]
 
-        if weapon == 'sword':
-            damage = 10
-        elif weapon == 'spear':
-            damage = 15
+        self.conn.sendall(f"attack {name} {damage}\n".encode())
+        ans, damage, hp = tuple(self.conn.recv(1024).decode().split())
+
+        if ans == 'empty':
+            print("No monster here")
+            return
+        elif ans == 'wrong_name':
+            print(f"No {name} here")
+            return
         else:
-            damage = 20
-
-
-        if hp < damage:
-            damage = hp
-        hp -= damage
-
-        print(f"Attacked {name},  damage {damage} hp")
-
-        if hp <= 0:
-            print(f"{name} died")
-            self.field[self.y][self.x] = 0
-        else:
-            print(f"{name} now has {hp}")
-            self.field[self.y][self.x]['hp'] = hp
-
+            print(f"Attacked {name},  damage {damage} hp")
+            if hp == '0':
+                print(f"{name} died")
+            else:
+                print(f"{name} now has {hp}")
 
     def complete_attack(self, text, line, begidx, endidx):
         res = shlex.split(line[:begidx], 0, 0)
@@ -215,11 +202,13 @@ class Mud(cmd.Cmd):
         elif res[-1] == 'with':
             return [c for c in self.weapons if c.startswith(text)]
 
-
     def do_EOF(self, args):
         return True
 
 
 if __name__ == "__main__":
     print("<<< Welcome to Python-MUD 0.1 >>>")
-    Mud().cmdloop()    
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        Mud(s).cmdloop()   
