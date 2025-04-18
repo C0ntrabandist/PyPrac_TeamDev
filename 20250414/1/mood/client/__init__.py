361cec9db2cae51cdd4c23c1c83c7804937ad5e7
LIO <ofleuxin@gmail.com>
2025-04-18 18:45:01 +0300

    Add initial MUD solution

---
+++ 20250414/1/mood/client/__init__.py
@@ -0,0 +1,84 @@
+"""Init file for client."""
+
+import cowsay
+import cmd
+import readline
+import shlex
+import time
+
+from ..common import jgsbat, prompt, weapons
+
+
+class Mood(cmd.Cmd):
+    """Class Mood shows the command line, autocomplete attack command and send commands to server."""
+
+    prompt = prompt
+
+    def __init__(self, conn, stdin):
+        """Initialize variables."""
+        super().__init__(stdin=stdin)
+
+        self.conn = conn
+
+        self.allowed_list = cowsay.list_cows()
+        self.user_list = {'jgsbat': jgsbat}
+
+    def precmd(self, line):
+        """Freeze console enter"""
+        time.sleep(1)
+        return super().precmd(line)
+
+    def do_up(self, args):
+        """Send to server message about moving up."""
+        self.conn.sendall("move 0 -1\n".encode())
+
+    def do_down(self, args):
+        """Send to server message about moving down."""
+        self.conn.sendall("move 0 1\n".encode())
+
+    def do_right(self, args):
+        """Send to server message about moving to the right."""
+        self.conn.sendall("move 1 0\n".encode())
+
+    def do_left(self, args):
+        """Send to server message about moving to the left."""
+        self.conn.sendall("move -1 0\n".encode())
+
+    def do_addmon(self, args):
+        """Send message about adding the monster."""
+        self.conn.sendall(("addmon " + args + "\n").encode())
+
+    def do_attack(self, args):
+        """Send message about attackin the monster."""
+        self.conn.sendall(("attack " + args + "\n").encode())
+
+    def do_sayall(self, args):
+        """Send message to all players."""
+        self.conn.sendall(("sayall " + args + "\n").encode())
+
+    def complete_attack(self, text, line, begidx, endidx):
+        """Complete attack line."""
+        res = shlex.split(line[:begidx], 0, 0)
+
+        if len(res) <= 1:
+            mon_list = list(self.user_list.keys()) + self.allowed_list
+            return [c for c in mon_list if c.startswith(text)]
+        elif res[-1] == 'with':
+            return [c for c in weapons if c.startswith(text)]
+
+    def do_EOF(self, args):
+        """End cmd activity."""
+        return True
+
+
+def recieve(cmd):
+    """Recieve the messages from server in another thread."""
+    while cmd.conn is not None:
+        data = ""
+
+        while len(new := cmd.conn.recv(1024)) == 1024:
+            data += new.decode()
+
+        data += new.decode()
+
+        print(f"\n{data.strip()}\n{cmd.prompt}{readline.get_line_buffer()}", end='', flush=True)
