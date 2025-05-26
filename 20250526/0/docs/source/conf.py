b6723042a035906dec7efcb1419271baabb340bc
LIO <ofleuxin@gmail.com>
2025-05-26 08:34:11 +0300

    Testing stuff

--- 20250526/0/docs/source/conf.py
+++ 20250526/0/docs/source/conf.py
@@ -17,14 +17,14 @@ author = 'jojo'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+#extensions = ['sphinx.ext.autodoc']
 
-templates_path = ['_templates']
-exclude_patterns = []
+#templates_path = ['_templates']
+#exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+#html_theme = 'alabaster'
+#html_static_path = ['_static']
